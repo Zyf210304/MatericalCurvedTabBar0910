c24de89b6fee05cc8f67721558f22717948adf7c
张亚飞 <672233580@qq.com>
2021-09-10 09:33:18 +0800

    Initial Commit

---
+++ MatericalCurvedTabBar0910/MatericalCurvedTabBar0910App.swift
@@ -0,0 +1,17 @@
+//
+//  MatericalCurvedTabBar0910App.swift
+//  MatericalCurvedTabBar0910
+//
+//  Created by 张亚飞 on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct MatericalCurvedTabBar0910App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
